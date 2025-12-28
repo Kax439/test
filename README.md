c400a6865240e37de2ff18f187deca05869df6cf
Kax439 <190744492+Kax439@users.noreply.github.com>
2025-12-28 13:01:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+Before you get started anything you need to create your NEW Application bot in [Discord Developer Portal](https://discord.com/developers/applications?new_application=true), Then copy and save your Application Bot's TOKEN (Please don't share this with anyone, Because whoever has it will have full access to your bot, for example they can Nuked your server!)
+**Make sure that your Bot have access to the Intents in the Portal.**
+
+Now you need to install JavaScript Environment Run (Node.js) in your Computer, Simply visit the https://nodejs.org and download the version 16.9 or higher, Because the Discord.js V14 is only valid in this version!
+
+After the download is complete, Open your code Editor, I recommend for you to use Visual Studio Code, You can download this Application on your computer in https://code.visualstudio.com
+
+Now Open your Toggle Panel with
+```
+Ctrl + J 
+```
+Select the Terminal Option and Type
+```
+npm init -y
+```
+for creating a place which name is `package.json` file to save your modules.
+
+<hr>
+
+Now you need to install all of required packages/libraries such as Discord.js module with these commands below and waiting for each of them:
+
+```
+npm install discord.js
+```
+and then
+```
+npm install @discordjs/voice
+```
+
+<hr>
+
+After the operation was processed with any error, please create a NEW file with
+```
+Ctrl + Alt + Windows + N
+```
+and set the name to anything you want such as: `index.js`, `bot.js`, `main.js`
+
+Then Upload the source of this repository along with your personal information for example: `botToken`, `prefix`, `voiceChannelId` and `rgbRoleId` in the [config.json](https://github.com/CalledMasih/Creating-Discord-Bot/blob/main/config.json) file.
+
+Finally enter the following command in your terminal to run/executing your file and wait for response in the console log...
+``` 
+node fileName.js
+```
+
+🎉 Congratulations, your bot was successfully online for provide lot's of amazing features!
+
+
+
+## Images
+Ping & Uptime Command 
+
+![Ping Command](https://github.com/CalledMasih/Creating-Discord-Bot/assets/100484009/e89b4627-4c00-43af-a0d5-b68ea2aac0f8)
+
+
+Join to a Voice Channel 
+
+![Join Voice Channel](https://github.com/CalledMasih/Creating-Discord-Bot/assets/100484009/1e96a076-da84-462d-af71-71b02b31fb92)
+
+
+RGB Role (Randomly changes the color of your provided role every **5** minutes)
+
+![RGB Role](https://github.com/CalledMasih/Creating-Discord-Bot/assets/100484009/e4fa79c9-2271-4f8a-bd64-c56e98537ac7)
+
+
+- If you have a problem while using this source make sure your to DM me on Discord: [CalledMasih](https://discord.com/users/901765485341859911), I will help you and fix your issue ASAP.
+- To get more information of How these codes work? you can simply check official guidelines in: https://discordjs.guide/ website.
+> Copyright (c): Masih 2022/12/16, All right reserved!
---
+++ index.js
@@ -0,0 +1,130 @@
+/* 
+Make Online your Bot in Discord.js V14
+Hope you Enjoy, Made with 🤍 by CalledMasih
+GitHub: https://github.com/calledmasih | Don't forget to ⭐
+Website: https://calledmasih.ir/
+Copyright Masih 2024 All Right Reserved!
+*/
+
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  ActivityType,
+} = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const config = require("./config.json");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
+
+client.on("ready", async () => {
+  const voiceChannel = client.channels.cache.get(config.voiceChannelId);
+  // Make Random status and Activity
+  async function changeStatus() {
+    const members = await voiceChannel.guild.members.fetch({
+      withPresences: true,
+    });
+    const onlineMembersCount = members.filter(
+      (m) =>
+        m.presence?.status === "online" ||
+        m.presence?.status === "idle" ||
+        m.presence?.status === "dnd"
+    ).size;
+    const activeMicsCount = members.filter((m) => m.voice.channel).size;
+    const activityName = [
+      `${voiceChannel.guild.name}`, // Guild Name
+      `${voiceChannel.guild.memberCount} Members`, // Guild Members Count
+      `${onlineMembersCount} Online`, // Online Guild Members Count
+      `${activeMicsCount} Active Mics`, // Total Members that Join to a Voice Channel
+    ];
+    const activityType = [
+      ActivityType.Competing,
+      ActivityType.Watching,
+      ActivityType.Watching,
+      ActivityType.Listening,
+    ];
+    const status = ["online", "idle", "dnd"];
+
+    const random = Math.floor(Math.random() * activityName.length);
+    client.user.setPresence({
+      status: status[random],
+      activities: [{ name: activityName[random], type: activityType[random] }],
+    });
+  }
+  // Refresh the Info every 15s
+  setInterval(changeStatus, 15_000);
+
+  // Join to a Voice Channel
+  function joinVoice() {
+    joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      selfDeaf: true, // Also you change it to false for unDeafen in Voice Channel
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+  }
+  setInterval(joinVoice, 30_000);
+
+  // RGB Role (Edit your Role with random color)
+  function editRole() {
+    const rgbRole = voiceChannel.guild.roles.cache.get(config.rgbRoleId);
+    rgbRole.edit({ color: "Random" });
+  }
+  // Edit the Role color every 5 minutes
+  setInterval(editRole, 300_000);
+
+  console.log(
+    `Logged in as ${client.user.tag}\nGitHub: https://github.com/calledmasih | Don't forget to ⭐`
+  );
+});
+
+client.on("messageCreate", async (message) => {
+  // Advanced Ping Command
+  if (message.content.startsWith(`${config.prefix}ping`)) {
+    await message.channel.sendTyping();
+    const pingEmbed = new EmbedBuilder()
+      .setTitle(client.user.username + " - Pong!")
+      .setThumbnail(client.user.displayAvatarURL({ size: 1024 }))
+      .addFields(
+        {
+          name: `🛰 Message Ping:`,
+          value: `${Date.now() - message.createdTimestamp}ms`,
+        },
+        {
+          name: `📊 API Latency:`,
+          value: `${Math.round(client.ws.ping)}ms`,
+        },
+        {
+          name: `⏳ Uptime:`,
+          value: `<t:${Math.round(
+            client.readyTimestamp / 1000
+          )}:f> | <t:${Math.round(client.readyTimestamp / 1000)}:R>`,
+        }
+      )
+      .setColor(message.guild.members.me.displayHexColor)
+
+      .setFooter({
+        text: `Requested by ${message.author.username}`,
+        iconURL: message.author.displayAvatarURL({ size: 1024 }),
+      })
+      .setTimestamp();
+
+    message.reply({
+      embeds: [pingEmbed],
+      allowedMentions: {
+        repliedUser: false,
+      },
+    });
+    message.react("✅");
+  }
+});
+
+// Log in to Your Application Bot
+client.login(config.botToken);
